5c7bbd0c8c00f2e54eb94e2f8def060daf4de3a3
SergioRosello <sergio-rosello@hotmail.com>
2020-07-20 12:32:32 +0200

    Landed for the first time in C kernel

--- asm/disk_load.asm
+++ asm/disk_load.asm
@@ -1,27 +1,46 @@
-; Load DH sectors to ES:BX from drive DL
+; load 'dh' sectors from drive 'dl' into ES:BX
 disk_load:
-  push dx             ; Store DX on stack so later we can recall 
-                      ; how many sectors where requested to be read,
-                      ; even if it is altered in the meantime.
-  mov ah, 0x02        ; BIOS read sector function
-  mov al, dh          ; Read DH sectors
-  mov ch, 0x00        ; Select cylinder 0
-  mov dh, 0x00        ; Select head 0
-  mov cl, 0x02        ; Start reading from second sector (i.e. 
-                      ; after the boot sector)
-  int 0x13            ; BIOS interrupt
+    pusha
+    ; reading from disk requires setting specific values in all registers
+    ; so we will overwrite our input parameters from 'dx'. Let's save it
+    ; to the stack for later use.
+    push dx
 
-  jc disk_error       ; Jump if error (i.e. carry flag set)
+    mov ah, 0x02 ; ah <- int 0x13 function. 0x02 = 'read'
+    mov al, dh   ; al <- number of sectors to read (0x01 .. 0x80)
+    mov cl, 0x02 ; cl <- sector (0x01 .. 0x11)
+                 ; 0x01 is our boot sector, 0x02 is the first 'available' sector
+    mov ch, 0x00 ; ch <- cylinder (0x0 .. 0x3FF, upper 2 bits in 'cl')
+    ; dl <- drive number. Our caller sets it as a parameter and gets it from BIOS
+    ; (0 = floppy, 1 = floppy2, 0x80 = hdd, 0x81 = hdd2)
+    mov dh, 0x00 ; dh <- head number (0x0 .. 0xF)
 
-  pop dx              ; Restore DX from the stack
-  cmp dh, al          ; if AL (sectors read) != DH (Sectors expected)
-  jne disk_error      ;   display error message
-  ret
+    ; [es:bx] <- pointer to buffer where the data will be stored
+    ; caller sets it up for us, and it is actually the standard location for int 13h
+    int 0x13      ; BIOS interrupt
+    jc disk_error ; if error (stored in the carry bit)
+
+    pop dx
+    cmp al, dh    ; BIOS also sets 'al' to the # of sectors read. Compare it.
+    jne sectors_error
+    popa
+    ret
+
 
 disk_error:
-  mov bx, DISK_ERROR_MESSAGE
-  call print_string
-  jmp $ 
+    mov bx, DISK_ERROR
+    call print_string
+    ; call print_nl
+    mov dh, ah ; ah = error code, dl = disk drive that dropped the error
+    ; call print_hex ; check out the code at http://stanislavs.org/helppc/int_13-1.html
+    jmp disk_loop
 
-; Variables
-DISK_ERROR_MESSAGE db "Disk read error!", 0
+sectors_error:
+    mov bx, SECTORS_ERROR
+    call print_string
+
+disk_loop:
+    jmp $
+
+DISK_ERROR: db "Disk read error", 0
+SECTORS_ERROR: db "Incorrect number of sectors read", 0
